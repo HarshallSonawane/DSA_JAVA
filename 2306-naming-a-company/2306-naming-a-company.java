eb1495cb0b64b618a9b9baa0f55a69550399b21b
Harshal Amol Sonawane <64686104+HarshallSonawane@users.noreply.github.com>
2023-02-09 12:37:40 +0530

    Time: 362 ms (61.90%), Space: 56.9 MB (42.86%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.java
@@ -0,0 +1,23 @@
+class Solution {
+       public long distinctNames(String[] ideas) {
+       List<String> arr[] = new ArrayList[26];               
+       long uniqueName = 0;                                     
+       for(int i = 0; i < 26; i++)                           
+           arr[i] = new ArrayList<>();
+
+       for(String s : ideas)
+           arr[s.charAt(0) - 'a'].add(s.substring(1));       
+       
+       for(int i = 0; i < 25; i++) {
+           for(int j = i + 1; j < 26; j++) {
+               
+               Set<String> set = new HashSet<>();           
+               
+               set.addAll(arr[i]);
+               set.addAll(arr[j]);
+               uniqueName += (arr[i].size() - set.size()) * (arr[j].size() - set.size());                      }
+       }
+
+       return uniqueName * 2;
+   }
+}
