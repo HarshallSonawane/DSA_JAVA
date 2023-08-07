236f7d97994b85b4644e00aa4fea11263941b441
Harshal Amol Sonawane <64686104+HarshallSonawane@users.noreply.github.com>
2023-08-07 14:20:09 +0530

    Time: 0 ms (100.00%), Space: 43 MB (75.43%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,15 +1,33 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
-        
-        int prev=cost[0],prev2=0,current;
-        for(int i=1;i<cost.length;i++){
-            current=Math.min(prev2+cost[i],prev+cost[i]);
-            prev2=prev;
-            prev=current;
+        int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2;i<dp.length;i++){
+            dp[i] = Math.min(dp[i-1],dp[i-2]) + cost[i];
         }
-        
-        return Math.min(prev,prev2);
-
+        return Math.min(dp[n-1], dp[n-2]);  
     }
+    
+//MEMOIZATION   
+    
+// return helper(cost, cost.length, new int[cost.length+1]);
+    
+//     public int helper(int[] arr, int n, int[] dp){
+        
+//         if(n == 1){return arr[0];}
+//         if(n == 2){return arr[0]+arr[1];}
+        
+//         if(dp[n]!=0){return dp[n];}
+        
+//         int f1 = helper(arr,n-1,dp);
+//         int f2 = helper(arr,n-2,dp);
+//         int ans = Math.min(f1,f2) + arr[n-1];
+//         dp[n] = ans;
+//         return ans;
+        
+//     }
+    
 }
