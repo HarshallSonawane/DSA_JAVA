cddcb85dba7d71d6502e6384346183b0295738db
Harshal Amol Sonawane <64686104+HarshallSonawane@users.noreply.github.com>
2023-09-11 09:04:45 +0530

    Time: 3 ms (95.43%), Space: 55.4 MB (68.42%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -10,12 +10,11 @@ class Solution {
         while(left<right){
             
             //idea is to find the min height from both and then use it to calculate area repeatedly
-            if(height[left] < height[right]){
-                len = right - left;
+            len = right - left;
+            if(height[left] < height[right]){ 
                 max = Math.max(max, height[left]*len);
                 left++;
             }else{
-                len = right - left;
                 max = Math.max(max, height[right]*len);
                 right--;
             }
