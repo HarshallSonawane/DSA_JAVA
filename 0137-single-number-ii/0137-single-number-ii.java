8cb464fb781d4442b6ec0ea2a370283550aeb3c6
Harshal Amol Sonawane <64686104+HarshallSonawane@users.noreply.github.com>
2023-09-15 10:47:57 +0530

    Time: 4 ms (60.31%), Space: 44 MB (39.66%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -4,15 +4,23 @@ class Solution {
         
         int res = 0;
         int sum, x;
+        
+        // traverse through all bits
         for(int i=0;i<INT_SIZE;i++){
             sum = 0;
+            
+            
             x = (1 << i);
+            
             for(int j=0;j<nums.length;j++){
+                
+                // to find out number of set bits
                 if((nums[j] & x)!= 0){
                     sum++;
                 }
             }
             
+            //if not multiple of three, we perform OR op with result
             if((sum%3) != 0){
                 res |= x;
             }
